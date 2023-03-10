50393bb8e2500a42fb32a5710aaeb2ccf70d394e
AnikRifat <reafatul@gmail.com>
2023-03-10 15:20:43 +0600

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,193 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <!-- Mirrored from colorlib.com/etc/searchf/colorlib-search-14/ by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 10 Mar 2023 09:02:16 GMT -->
+
+    <head>
+
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="description" content="Colrolib Templates">
+        <meta name="author" content="colorlib.com">
+        <meta name="keywords" content="Colrolib Templates">
+
+        <title>Au Form Wizard</title>
+
+        <link href="asset/vendor/mdi-font/asset/css/material-design-iconic-font.min.css" rel="stylesheet" media="all">
+        <link href="asset/vendor/font-awesome-4.7/asset/css/font-awesome.min.css" rel="stylesheet" media="all">
+
+        <style id="" media="all">
+            @font-face {
+                font-family: 'Roboto';
+                font-style: italic;
+                font-weight: 100;
+                font-display: swap;
+                src: url(https://colorlib.com/fonts.gstatic.com/s/roboto/v30/KFOiCnqEu92Fr1Mu51QrEzAdKg.ttf) format('truetype');
+            }
+
+            @font-face {
+                font-family: 'Roboto';
+                font-style: italic;
+                font-weight: 300;
+                font-display: swap;
+                src: url(https://colorlib.com/fonts.gstatic.com/s/roboto/v30/KFOjCnqEu92Fr1Mu51TjASc6CsE.ttf) format('truetype');
+            }
+
+            @font-face {
+                font-family: 'Roboto';
+                font-style: italic;
+                font-weight: 400;
+                font-display: swap;
+                src: url(https://colorlib.com/fonts.gstatic.com/s/roboto/v30/KFOkCnqEu92Fr1Mu51xIIzc.ttf) format('truetype');
+            }
+
+            @font-face {
+                font-family: 'Roboto';
+                font-style: italic;
+                font-weight: 500;
+                font-display: swap;
+                src: url(https://colorlib.com/fonts.gstatic.com/s/roboto/v30/KFOjCnqEu92Fr1Mu51S7ACc6CsE.ttf) format('truetype');
+            }
+
+            @font-face {
+                font-family: 'Roboto';
+                font-style: italic;
+                font-weight: 700;
+                font-display: swap;
+                src: url(https://colorlib.com/fonts.gstatic.com/s/roboto/v30/KFOjCnqEu92Fr1Mu51TzBic6CsE.ttf) format('truetype');
+            }
+
+            @font-face {
+                font-family: 'Roboto';
+                font-style: italic;
+                font-weight: 900;
+                font-display: swap;
+                src: url(https://colorlib.com/fonts.gstatic.com/s/roboto/v30/KFOjCnqEu92Fr1Mu51TLBCc6CsE.ttf) format('truetype');
+            }
+
+            @font-face {
+                font-family: 'Roboto';
+                font-style: normal;
+                font-weight: 100;
+                font-display: swap;
+                src: url(https://colorlib.com/fonts.gstatic.com/s/roboto/v30/KFOkCnqEu92Fr1MmgVxIIzc.ttf) format('truetype');
+            }
+
+            @font-face {
+                font-family: 'Roboto';
+                font-style: normal;
+                font-weight: 300;
+                font-display: swap;
+                src: url(https://colorlib.com/fonts.gstatic.com/s/roboto/v30/KFOlCnqEu92Fr1MmSU5fBBc9.ttf) format('truetype');
+            }
+
+            @font-face {
+                font-family: 'Roboto';
+                font-style: normal;
+                font-weight: 400;
+                font-display: swap;
+                src: url(https://colorlib.com/fonts.gstatic.com/s/roboto/v30/KFOmCnqEu92Fr1Mu4mxP.ttf) format('truetype');
+            }
+
+            @font-face {
+                font-family: 'Roboto';
+                font-style: normal;
+                font-weight: 500;
+                font-display: swap;
+                src: url(https://colorlib.com/fonts.gstatic.com/s/roboto/v30/KFOlCnqEu92Fr1MmEU9fBBc9.ttf) format('truetype');
+            }
+
+            @font-face {
+                font-family: 'Roboto';
+                font-style: normal;
+                font-weight: 700;
+                font-display: swap;
+                src: url(https://colorlib.com/fonts.gstatic.com/s/roboto/v30/KFOlCnqEu92Fr1MmWUlfBBc9.ttf) format('truetype');
+            }
+
+            @font-face {
+                font-family: 'Roboto';
+                font-style: normal;
+                font-weight: 900;
+                font-display: swap;
+                src: url(https://colorlib.com/fonts.gstatic.com/s/roboto/v30/KFOlCnqEu92Fr1MmYUtfBBc9.ttf) format('truetype');
+            }
+        </style>
+
+        <link href="asset/vendor/select2/select2.min.css" rel="stylesheet" media="all">
+        <link href="asset/vendor/datepicker/daterangepicker.css" rel="stylesheet" media="all">
+
+        <link href="asset/css/main.css" rel="stylesheet" media="all">
+        <meta name="robots" content="noindex, follow">
+        <script nonce="3ebc3b8d-f251-4fdc-946d-364985f6c3fb">
+            (function(w,d){!function(bv,bw,bx,by){bv[bx]=bv[bx]||{};bv[bx].executed=[];bv.zaraz={deferred:[],listeners:[]};bv.zaraz.q=[];bv.zaraz._f=function(bz){return function(){var bA=Array.prototype.slice.call(arguments);bv.zaraz.q.push({m:bz,a:bA})}};for(const bB of["track","set","debug"])bv.zaraz[bB]=bv.zaraz._f(bB);bv.zaraz.init=()=>{var bC=bw.getElementsByTagName(by)[0],bD=bw.createElement(by),bE=bw.getElementsByTagName("title")[0];bE&&(bv[bx].t=bw.getElementsByTagName("title")[0].text);bv[bx].x=Math.random();bv[bx].w=bv.screen.width;bv[bx].h=bv.screen.height;bv[bx].j=bv.innerHeight;bv[bx].e=bv.innerWidth;bv[bx].l=bv.location.href;bv[bx].r=bw.referrer;bv[bx].k=bv.screen.colorDepth;bv[bx].n=bw.characterSet;bv[bx].o=(new Date).getTimezoneOffset();if(bv.dataLayer)for(const bI of Object.entries(Object.entries(dataLayer).reduce(((bJ,bK)=>({...bJ[1],...bK[1]})))))zaraz.set(bI[0],bI[1],{scope:"page"});bv[bx].q=[];for(;bv.zaraz.q.length;){const bL=bv.zaraz.q.shift();bv[bx].q.push(bL)}bD.defer=!0;for(const bM of[localStorage,sessionStorage])Object.keys(bM||{}).filter((bO=>bO.startsWith("_zaraz_"))).forEach((bN=>{try{bv[bx]["z_"+bN.slice(7)]=JSON.parse(bM.getItem(bN))}catch{bv[bx]["z_"+bN.slice(7)]=bM.getItem(bN)}}));bD.referrerPolicy="origin";bD.src="../../../cdn-cgi/zaraz/sd0d9.js?z="+btoa(encodeURIComponent(JSON.stringify(bv[bx])));bC.parentNode.insertBefore(bD,bC)};["complete","interactive"].includes(bw.readyState)?zaraz.init():bv.addEventListener("DOMContentLoaded",zaraz.init)}(w,d,"zarazData","script");})(window,document);
+        </script>
+    </head>
+
+    <body>
+        <div class="page-wrapper bg-color-1 p-t-395 p-b-120">
+            <div class="wrapper wrapper--w1070">
+                <div class="card card-7">
+                    <div class="card-body">
+                        <form class="form" method="POST" action="#">
+                            <div class="input-group input--large">
+                                <label class="label">going to</label>
+                                <input class="input--style-1" type="text" placeholder="Destination, hotel name"
+                                  name="going">
+                            </div>
+                            <div class="input-group input--medium">
+                                <label class="label">Check-In</label>
+                                <input class="input--style-1" type="text" name="checkin" placeholder="mm/dd/yyyy"
+                                  id="input-start">
+                            </div>
+                            <div class="input-group input--medium">
+                                <label class="label">Check-Out</label>
+                                <input class="input--style-1" type="text" name="checkout" placeholder="mm/dd/yyyy"
+                                  id="input-end">
+                            </div>
+                            <div class="input-group input--medium">
+                                <label class="label">guests</label>
+                                <div class="input-group-icon js-number-input">
+                                    <div class="icon-con">
+                                        <span class="plus">+</span>
+                                        <span class="minus">-</span>
+                                    </div>
+                                    <input class="input--style-1 quantity" type="text" name="guests" value="2 Guests">
+                                </div>
+                            </div>
+                            <button class="btn-submit" type="submit">search</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <script src="asset/vendor/jquery/jquery.min.js"></script>
+
+        <script src="asset/vendor/select2/select2.min.js"></script>
+        <script src="asset/vendor/jquery-validate/jquery.validate.min.js"></script>
+        <script src="asset/vendor/bootstrap-wizard/bootstrap.min.js"></script>
+        <script src="asset/vendor/bootstrap-wizard/jquery.bootstrap.wizard.min.js"></script>
+        <script src="asset/vendor/datepicker/moment.min.js"></script>
+        <script src="asset/vendor/datepicker/daterangepicker.js"></script>
+
+        <script src="asset/js/global.js"></script>
+
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-23581568-13"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-23581568-13');
+        </script>
+        <script defer
+          src="https://static.cloudflareinsights.com/beacon.min.js/vaafb692b2aea4879b33c060e79fe94621666317369993"
+          integrity="sha512-0ahDYl866UMhKuYcW078ScMalXqtFJggm7TmlUtp0UlD4eQk0Ixfnm5ykXKvGJNFjLMoortdseTfsRT8oCfgGA=="
+          data-cf-beacon='{"rayId":"7a5a60b3cb2af4ac","token":"cd0b4b3a733644fc843ef0b185f98241","version":"2023.2.0","si":100}'
+          crossorigin="anonymous"></script>
+    </body>
+
+    <!-- Mirrored from colorlib.com/etc/searchf/colorlib-search-14/ by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 10 Mar 2023 09:02:24 GMT -->
+
+</html>
